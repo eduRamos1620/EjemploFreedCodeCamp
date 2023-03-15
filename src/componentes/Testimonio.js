4f322171275638a905fdb6c7ecb3fa0d2ed96ba0
azazel <eduardoramosrosales16@gmail.com>
2023-03-15 12:25:25 -0600

    Css del componente

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,4 +1,5 @@
 import React from "react";
+import '../componentes/styles/Testimonio.css';
 
 function Testimonio(){
     return(
@@ -9,8 +10,8 @@ function Testimonio(){
                 alt='Foto atardecer'/>
             <div className='contenedor-texto-testimonio'>
                 <p className='nombre-testimonio'>Atardecer</p>
-                <p className='nombre-testimonio'>Bonito atardecer</p>
-                <p className='texto-testimonio'>aqui va mucho texto</p>
+                <p className='cargo-testimonio'>Bonito atardecer</p>
+                <p className='texto-testimonio'>Hoy es un día bonito para amanecer de buenas Para cantarle al amor para olvidar mis penas Para mí eres la reina en esta colmena Me trajo a la cama mi café y un plato de avena Preparo dos clericots y que importa la hora De tanto que me inspiras eres cómo mi editora Yo soy desierto y tu agua en esta cantimplora Mi cora ríe hace rato que no llora</p>
             </div>
         </div>
     );
