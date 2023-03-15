7e43a1102873016c7ff0db3f54917c87abaa8646
azazel <eduardoramosrosales16@gmail.com>
2023-03-15 13:09:43 -0600

    ultimos detalles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Testimonio from './componentes/Testimonio.js'
+import Testimonio from './componentes/Testimonio.jsx'
 
 function App() {
   return (
--- src/componentes/Testimonio.jsx
+++ src/componentes/Testimonio.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import '../componentes/styles/Testimonio.css';
 
+//se crea el contenedor para introducir los testimonios siendo reutilizable
 function Testimonio(props){
     return(
         <div className='contenedor-testimonio'>
@@ -9,7 +10,7 @@ function Testimonio(props){
                 src={require(`../imagenes/testimonio-${props.imagen}.jpg`)}
                 alt='Foto atardecer'/>
             <div className='contenedor-texto-testimonio'>
-                <p className='nombre-testimonio'>{props.nombre} en {props.pais}</p>
+                <p className='nombre-testimonio'><strong>{props.nombre}</strong> en {props.pais}</p>
                 <p className='cargo-testimonio'>{props.cargo} {props.empresa}</p>
                 <p className='texto-testimonio'>{props.testimonio}</p>
             </div>
