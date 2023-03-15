88368a1f517d11c364da43026b42f85e0453ea32
azazel <eduardoramosrosales16@gmail.com>
2023-03-14 23:44:53 -0600

    Eliminando archivos inecesarios para este proyecto y creando estructura del componente

---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Testimonio(){
+    return(
+        <div className='contenedor-testimonio'>
+            <img 
+                className='imagen-testimonio' 
+                src={require('../imagenes/testimonio-atardecer.jpg')}
+                alt='Foto atardecer'/>
+            <div className='contenedor-texto-testimonio'>
+                <p className='nombre-testimonio'>Atardecer</p>
+                <p className='nombre-testimonio'>Bonito atardecer</p>
+                <p className='texto-testimonio'>aqui va mucho texto</p>
+            </div>
+        </div>
+    );
+}
