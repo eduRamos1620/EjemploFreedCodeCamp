ed40bf2794fc3845746f36e72daf629a3f42a113
azazel <eduardoramosrosales16@gmail.com>
2023-03-15 00:07:50 -0600

    Empezando a agregar estilos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimonio from './componentes/Testimonio.js'
 
 function App() {
   return (
     <div className="App">
-      
+      <div className='contenedor-principal'>
+        <h1>Esto es lo que dicen nuestros al umnos</h1>
+        <Testimonio />
+      </div>
     </div>
   );
 }
